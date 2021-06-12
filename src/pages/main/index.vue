1d3dfd641de0e0df000a1ed9b8a2e44e4d0f1d1f
Yukilwc <misaka1033@gmail.com>
2021-06-12 17:07:13 +0800

    u

---
+++ src/pages/main/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>
