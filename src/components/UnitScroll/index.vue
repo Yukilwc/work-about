c92238a3299a4ed9fd8a87a226ae50dbb5830ceb
Yukilwc <misaka1033@gmail.com>
2021-10-17 21:17:12 +0800

    u

--- src/components/UnitScroll/index.vue
+++ src/components/UnitScroll/index.vue
@@ -1,18 +1,77 @@
 <template>
-  <div>数字滚动</div>
+  <div class="unit-container">
+    <div class="unit-wrapper" :style="wrapperStyle">
+      <div class="list">
+        <div
+          class="item"
+          v-for="(item, index) in list"
+          :key="index"
+          :style="itemStyle"
+        >
+          {{ item }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, onMounted } from "vue";
+import { computed, defineComponent, onMounted, toRefs } from "vue";
 
 export default defineComponent({
+  props: {
+    w: {
+      type: String,
+      default: "auto",
+    },
+    h: {
+      type: String,
+      default: "1.5em",
+    },
+  },
   components: {},
   setup(props, context) {
+    const wrapperStyle = computed(() => {
+      let { w, h } = toRefs(props);
+      let res = `width: ${w.value};height: ${h.value}`;
+      console.log("==========res", res);
+      return res;
+    });
+    const itemStyle = computed(() => {
+      let { w, h } = toRefs(props);
+      let res = `width: ${w.value};height: ${h.value}`;
+      console.log("==========res", res);
+      return res;
+    });
+
     onMounted(() => {});
-    return {};
+    return {
+      list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      wrapperStyle,
+      itemStyle,
+    };
   },
 });
 </script>
 
 <style scoped lang="scss">
+.unit-container {
+  display: inline-block;
+  .unit-wrapper {
+    font-size: inherit;
+    font-weight: inherit;
+    display: inline-block;
+    position: relative;
+    overflow: hidden;
+    .list {
+      transition: all 1s ease;
+      // transform: translateY(-8em);
+      .item {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  }
+}
 </style>
--- src/pages/showPage/show-2.vue
+++ src/pages/showPage/show-2.vue
@@ -1,11 +1,23 @@
 <template>
   <div>展示</div>
   <div class="">数字滚动</div>
-  <UnitScroll></UnitScroll>
+  <div class="unit-scroll-comp" style="width: 1200px; margin: auto">
+    <UnitScroll
+      w="auto"
+      h="3em"
+      :start="options.start"
+      :end="options.end"
+      :dur="options.dur"
+      :page="options.page"
+    ></UnitScroll>
+  </div>
+  <div class="">
+    <el-button type="primary" @click="startClick">启动</el-button>
+  </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, reactive } from "vue";
 import UnitScroll from "@/components/UnitScroll/index.vue";
 
 export default defineComponent({
@@ -14,10 +26,23 @@ export default defineComponent({
   },
   setup(props, context) {
     onMounted(() => {});
-    return {};
+    let options = reactive({
+      start: 0,
+      end: 9,
+      dur: 1000,
+      page: 0,
+    });
+    return {
+      startClick: () => {},
+      options,
+    };
   },
 });
 </script>
 
 <style scoped lang="scss">
+.unit-scroll-comp {
+  font-size: 30px;
+  font-weight: bold;
+}
 </style>
