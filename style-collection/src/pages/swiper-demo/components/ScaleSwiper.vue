3b7a35c01b7163c9dad0e1d934ae391abfc0e901
Yukilwc <misaka1033@gmail.com>
2022-01-25 01:48:15 +0800

    u

--- style-collection/src/pages/swiper-demo/components/ScaleSwiper.vue
+++ style-collection/src/pages/swiper-demo/components/ScaleSwiper.vue
@@ -18,7 +18,7 @@
             <div class></div>
           </div>
         </div>
-        <div class="swiper-slide">
+        <!-- <div class="swiper-slide">
           <div class="item-container w-[634px] h-[376px] bg-stone-500">
             <div class></div>
           </div>
@@ -27,7 +27,7 @@
           <div class="item-container w-[634px] h-[376px] bg-stone-900">
             <div class></div>
           </div>
-        </div>
+        </div>-->
       </div>
       <!-- If we need pagination -->
       <div class="swiper-pagination"></div>
@@ -58,14 +58,17 @@ const initSwiper = () => {
     on: {
       setTranslate: function (swiper, translate) {
         const slides = swiper.slides
-        console.log('==========swiper.progress', swiper)
+        // console.log('==========swiper.progress', swiper)
         slides.forEach((slide, index) => {
           const progress = Reflect.get(slide, 'progress')
-          let styleStr = `opacity:'';background:'';transform:scale(${(1 - Math.abs(progress) / 4)})`
+          let styleStr = `opacity:'';background:'';transform:scale(${(1 - Math.abs(progress) / 5)})`
           slide.setAttribute("style", styleStr);
+          console.log('========== styleStr', styleStr)
+          console.log('========== progress', progress)
         })
       },
-      setTransition: function (transition) {
+      setTransition: function (swiper, transition) {
+        console.log('==========set transition', transition)
         for (var i = 0; i < this.slides.length; i++) {
           var slide = this.slides.eq(i)
           slide.transition(transition);
