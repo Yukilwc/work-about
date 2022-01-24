5328d67d606034c7e6160013a0ee0fdcc463f013
Yukilwc <misaka1033@gmail.com>
2022-01-25 01:39:53 +0800

    u

---
+++ style-collection/src/pages/swiper-demo/components/ScaleSwiper.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="text-[24px] font-bold text-left">ScaleSwiper</div>
+  <div class="w-full overflow-hidden">
+    <div class="swiper-container mx-auto w-full overflow-hidden" ref="containerRef">
+      <div class="swiper-wrapper w-full">
+        <div class="swiper-slide">
+          <div class="item-container w-[634px] h-[376px] bg-orange-200">
+            <div class></div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="item-container w-[634px] h-[376px] bg-orange-600">
+            <div class></div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="item-container w-[634px] h-[376px] bg-red-400">
+            <div class></div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="item-container w-[634px] h-[376px] bg-stone-500">
+            <div class></div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="item-container w-[634px] h-[376px] bg-stone-900">
+            <div class></div>
+          </div>
+        </div>
+      </div>
+      <!-- If we need pagination -->
+      <div class="swiper-pagination"></div>
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts' setup>
+import Swiper, { Navigation, Pagination, Autoplay, } from "swiper"
+import 'swiper/css/bundle';
+import { onMounted, ref } from "vue";
+const containerRef = ref<HTMLElement>()
+onMounted(() => {
+  initSwiper()
+})
+const initSwiper = () => {
+  if (!containerRef.value) return
+  var swiper = new Swiper(containerRef.value, {
+    loop: true,
+    speed: 2500,
+    slidesPerView: 3,
+    // spaceBetween: 30,
+    centeredSlides: true,
+    watchSlidesProgress: true,
+    on: {
+      setTranslate: function (swiper, translate) {
+        const slides = swiper.slides
+        console.log('==========swiper.progress', swiper)
+        slides.forEach((slide, index) => {
+          const progress = Reflect.get(slide, 'progress')
+          let styleStr = `opacity:'';background:'';transform:scale(${(1 - Math.abs(progress) / 4)})`
+          slide.setAttribute("style", styleStr);
+        })
+      },
+      setTransition: function (transition) {
+        for (var i = 0; i < this.slides.length; i++) {
+          var slide = this.slides.eq(i)
+          slide.transition(transition);
+        }
+      },
+      transitionEnd: () => {
+        console.log('==========transitionEnd',)
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
+</script>
+
+<style scoped lang="scss">
+.swiper-slide {
+  transition: all 0.8s;
+  width: 634px;
+}
+</style>
--- style-collection/src/pages/swiper-demo/index.vue
+++ style-collection/src/pages/swiper-demo/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="font-bold w-full text-[30px]">swiper demo</div>
+  <ScaleSwiper></ScaleSwiper>
 </template>
 
 <script lang='ts' setup>
+import ScaleSwiper from "./components/ScaleSwiper.vue"
 </script>
 
 <style scoped lang="scss">
